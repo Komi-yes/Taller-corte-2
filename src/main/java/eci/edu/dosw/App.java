35453fcf0d5af035297c2dd322400429329d3ddd
komi_yes <danielpalacios04@gmail.com>
2025-10-20 19:29:27 -0500

    Setup project with Spring Boot, MongoDB, Swagger

---
+++ src/main/java/eci/edu/dosw/App.java
@@ -0,0 +1,19 @@
+package eci.edu.dosw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class App {
+
+    private static ConfigurableApplicationContext context;
+
+    public static void main(String[] args) {
+        context = SpringApplication.run(App.class, args);
+    }
+
+    static ConfigurableApplicationContext getContext() {
+        return context;
+    }
+}
---
+++ src/main/java/eci/edu/dosw/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package eci.edu.dosw.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Master chef API")
+                        .version("1.0")
+                        .description("Documentación de Master chef API"));
+    }
+}
