74e83d9c5add28f36f7fdc9d6b4808a65569ad04
komi_yes <danielpalacios04@gmail.com>
2025-10-31 19:23:06 -0500

    refactor(core): Use Lombok annotations for recipe models
    This commit refactors the JuryRecipe, ParticipantRecipe, and ViewerRecipe classes to use Lombok's @EqualsAndHashCode annotation. Also fixes package declaration and imports in several files.

--- src/main/java/eci/edu/dosw/models/ParticipantRecipe.java
+++ src/main/java/eci/edu/dosw/models/ParticipantRecipe.java
@@ -3,9 +3,11 @@ package eci.edu.dosw.models;
 import eci.edu.dosw.models.enums.ChefType;
 import java.util.List;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @Document(collection = "recipes")
